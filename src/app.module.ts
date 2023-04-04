b768f5b5e1b6af0dd4e7fd156bccc728fd9794fa
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-04-04 12:27:54 +0545

    Initialized TypeORM with its configuration in main app module with optional configuration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
+// Import Third-Party Modules
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Import User-Defined Modules
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 
+/**
+ * This module is the root module which acts as starting point to build the whole
+ * application graph.
+ */
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: false,
+      logging: false,
     }),
   ],
   controllers: [AppController],
