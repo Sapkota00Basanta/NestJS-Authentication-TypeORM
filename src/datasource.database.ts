b3a81042c5ae7d429276519498c93e476bdcfc99
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-04-10 21:39:02 +0545

    Added environment variable with configuration for usage

--- src/datasource.database.ts
+++ src/datasource.database.ts
@@ -1,25 +1,25 @@
 // Import Third-Party Modules
-import { DataSource, DataSourceOptions } from 'typeorm';
+import * as typeorm from 'typeorm';
+
+// Import User-Defined Modules
+import { configurationValues } from './config';
 
 /**
  * This is datasource options which is used by TypeORM to connect with database.
  */
-export const mainDataSourceOptions: DataSourceOptions = {
-  type: 'mssql',
-  host: process.env.DATABASE_HOST ?? 'localhost',
-  port: Number(process.env.DATABASE_PORT) ?? 1433,
-  username: process.env.DATABASE_USER ?? 'SA',
-  password: process.env.DATABASE_PASSWORD ?? 'RootPass@123',
-  database: process.env.DATABASE_NAME ?? 'nest_auth',
+export const mainDataSourceOptions: typeorm.DataSourceOptions = {
+  type: 'postgres',
+  host: configurationValues.database.host,
+  port: Number(configurationValues.database.port),
+  username: configurationValues.database.username,
+  password: String(configurationValues.database.password),
+  database: configurationValues.database.database,
   //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   entities: ['dist/**/entities/*.entity.{ts,js}'],
   migrations: ['dist/migration/**/*.{js,ts}'],
   subscribers: ['src/subscriber/**/*.{js,ts}'],
-  options: {
-    encrypt: false,
-  },
 };
 
-const dataSource = new DataSource(mainDataSourceOptions);
+const dataSource = new typeorm.DataSource(mainDataSourceOptions);
 export default dataSource;
