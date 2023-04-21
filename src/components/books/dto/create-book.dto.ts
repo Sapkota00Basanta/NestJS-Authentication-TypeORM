b92b9fe273fa9c4374aee5fb89b8e57a866a98f1
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-04-22 00:04:11 +0545

    Validation implemented using class-validator and global pipes

--- src/components/books/dto/create-book.dto.ts
+++ src/components/books/dto/create-book.dto.ts
@@ -1,5 +1,9 @@
 // Import Third-Party Modules
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsArray } from 'class-validator';
+
+// Import User-Defined Modules
+import { Users } from 'src/components/users/entities/user.entity';
 
 /**
  * This module is a contract with which data is being transferred.
@@ -10,23 +14,37 @@ export class CreateBookDto {
     type: String,
     description: 'Title of book resource.',
   })
+  @IsNotEmpty({ message: 'Title field of book cannot be empty.' })
+  @IsString({ message: 'Type of Book Title must be String.' })
   title: string;
 
   @ApiProperty({
     type: String,
     description: 'Generalized description about the book.',
   })
+  @IsNotEmpty({ message: 'Description about the book cannot be empty.' })
+  @IsString({ message: 'Type of Book Description must be String.' })
   description: string;
 
   @ApiProperty({
     type: String,
     description: 'Thumbnail property of book resource.',
   })
+  @IsNotEmpty({ message: 'Thumbnail field cannot be empty.' })
+  @IsString({ message: 'Type of Book thumbnail must be string.' })
   thumbnail: string;
 
   @ApiProperty({
     type: [String],
     description: 'Author of the book resource.',
   })
+  @IsNotEmpty({ message: 'Author filed cannot be empty.' })
+  @IsArray({ message: 'Type of Book Author must be an Array of string.' })
   author: Array<string>;
+
+  @ApiProperty({
+    type: Users,
+    description: 'User who is viewing the books resource.',
+  })
+  user: Users;
 }
